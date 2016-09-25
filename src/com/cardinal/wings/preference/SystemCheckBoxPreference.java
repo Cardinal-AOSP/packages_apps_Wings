245a2ea129cd4809fa078e2bef28636422060191
Alex Cruz <mazdarider23@gmail.com>
2016-09-25 12:25:48 +0000

    Update switch and checkbox preferences
    Fixes casting issues like 'Cannot cast from Preference to SwitchPreference'
    
    Don't know why but Google forced this on us so be it. Change is good :-)
    
    Change-Id: I6bf417e56c4629355f9f544726d21ce4f71d62df

--- src/com/cardinal/wings/preference/SystemCheckBoxPreference.java
+++ src/com/cardinal/wings/preference/SystemCheckBoxPreference.java
@@ -17,7 +17,7 @@
 package com.cardinal.wings.preference;
 
 import android.content.Context;
-import android.preference.CheckBoxPreference;
+import android.support.v7.preference.CheckBoxPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
