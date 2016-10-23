kimjisoo/packages_apps_Tweaks

DUI: Initial N checkin
Change-Id: I98e639868eeb4c5d5496f73cd6c67345c7c2e57b

src/com/dirtyunicorns/dutweaks/CustomActionListAdapter.java
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.dirtyunicorns.dutweaks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.utils.du.ActionHandler;
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}

src/com/dirtyunicorns/dutweaks/IconPickerGallery.java
@@ -1,0 +1,105 @@
+/**
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.dirtyunicorns.dutweaks;
+
+import java.io.File;
+
+import com.android.internal.utils.du.ImageHelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.UserHandle;
+import android.widget.Toast;
+
+import com.android.settings.R;
+
+/**
+ * So we can capture image selection in DUSystemReceiver
+ */
+public class IconPickerGallery extends Activity {
+    public static String TAG = IconPickerGallery.class.getSimpleName();
+    public static final String INTENT_GALLERY_PICKER = "intent_gallery_picker";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(intent, 69);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == 69) {
+            Bitmap b = null;
+            try {
+                b = ImageHelper.getBitmapFromUri(this, data.getData());
+            } catch (Exception e) {
+                Toast.makeText(this, getString(R.string.invalid_icon_from_uri), Toast.LENGTH_SHORT)
+                        .show();
+                sendCancelResultAndFinish();
+            }
+            if (b != null) {
+                File dir = new File(Environment.getExternalStorageDirectory() + File.separator
+                        + "dui_icons");
+                dir.mkdirs();
+                String fileName = "dui_icons_"
+                        + String.valueOf(System.currentTimeMillis());
+                ;
+                Uri newUri = ImageHelper.addBitmapToStorage(dir, fileName, b);
+                if (newUri == null) {
+                    Toast.makeText(this, getString(R.string.invalid_icon_from_uri),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    sendCancelResultAndFinish();
+                } else {
+                    Intent resultIntent = new Intent();
+                    resultIntent.setAction(INTENT_GALLERY_PICKER);
+                    resultIntent.putExtra("result", Activity.RESULT_OK);
+                    resultIntent.putExtra("uri", newUri.toString());
+                    sendBroadcastAsUser(resultIntent, UserHandle.CURRENT);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.invalid_icon_from_uri), Toast.LENGTH_SHORT)
+                        .show();
+                sendCancelResultAndFinish();
+            }
+        } else {
+            sendCancelResultAndFinish();
+        }
+    }
+
+    private void sendCancelResultAndFinish() {
+        Intent intent = new Intent(INTENT_GALLERY_PICKER);
+        intent.putExtra("result", Activity.RESULT_CANCELED);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+}

src/com/dirtyunicorns/dutweaks/preference/ActionPreference.java
@@ -1,0 +1,134 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Simple preference class implementing ActionHolder interface to assign
+ * actions to buttons. It is ABSOLUTELY IMPERITIVE that the preference
+ * key is identical to the target ConfigMap tag in ActionConstants 
+ */
+
+package com.dirtyunicorns.dutweaks.preference;
+
+import java.util.Map;
+
+import com.android.internal.utils.du.ActionConstants.ConfigMap;
+import com.android.internal.utils.du.ActionConstants.Defaults;
+import com.android.internal.utils.du.ActionHolder;
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.internal.utils.du.Config.ButtonConfig;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+
+public class ActionPreference extends Preference implements ActionHolder {
+    private Defaults mDefaults;
+    private ConfigMap mMap;
+    private ActionConfig mAction;
+    private ActionConfig mDefaultAction;
+
+    public ActionPreference(Context context) {
+        this(context, null);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs);
+    }
+
+    @Override
+    public String getTag() {
+        return this.getKey();
+    }
+
+    @Override
+    public void setTag(String tag) {
+        this.setKey(tag);
+    }
+
+    @Override
+    public Defaults getDefaults() {
+        return mDefaults;
+    }
+
+    @Override
+    public void setDefaults(Defaults defaults) {
+        mDefaults = defaults;
+        final String tag = this.getKey();
+        for (Map.Entry<String, ConfigMap> entry : defaults.getActionMap().entrySet()) {
+            if (((String) entry.getKey()).equals(tag)) {
+                mMap = entry.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public ConfigMap getConfigMap() {
+        return mMap;
+    }
+
+    @Override
+    public void setConfigMap(ConfigMap map) {
+        mMap = map;
+    }
+
+    @Override
+    public ButtonConfig getButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setButtonConfig(ButtonConfig button) {
+    }
+
+    @Override
+    public ActionConfig getActionConfig() {
+        return mAction;
+    }
+
+    @Override
+    public void setActionConfig(ActionConfig action) {
+        mAction = action;
+        this.setSummary(action.getLabel());
+    }
+
+    @Override
+    public ButtonConfig getDefaultButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultButtonConfig(ButtonConfig button) {
+
+    }
+
+    @Override
+    public ActionConfig getDefaultActionConfig() {
+        return mDefaultAction;
+    }
+
+    @Override
+    public void setDefaultActionConfig(ActionConfig action) {
+        mDefaultAction = action;
+    }
+}

Cardinal-AOSP/packages_apps_Wings

src/com/cardinal/wings/CustomActionListAdapter.java
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.cardinal.wings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.utils.du.ActionHandler;
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}

src/com/cardinal/wings/IconPickerGallery.java
@@ -1,0 +1,105 @@
+/**
+ * Copyright (C) 2016 The DirtyUnicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.cardinal.wings;
+
+import java.io.File;
+
+import com.android.internal.utils.du.ImageHelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.UserHandle;
+import android.widget.Toast;
+
+import com.android.settings.R;
+
+/**
+ * So we can capture image selection in DUSystemReceiver
+ */
+public class IconPickerGallery extends Activity {
+    public static String TAG = IconPickerGallery.class.getSimpleName();
+    public static final String INTENT_GALLERY_PICKER = "intent_gallery_picker";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        startActivityForResult(intent, 69);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK && requestCode == 69) {
+            Bitmap b = null;
+            try {
+                b = ImageHelper.getBitmapFromUri(this, data.getData());
+            } catch (Exception e) {
+                Toast.makeText(this, getString(R.string.invalid_icon_from_uri), Toast.LENGTH_SHORT)
+                        .show();
+                sendCancelResultAndFinish();
+            }
+            if (b != null) {
+                File dir = new File(Environment.getExternalStorageDirectory() + File.separator
+                        + "dui_icons");
+                dir.mkdirs();
+                String fileName = "dui_icons_"
+                        + String.valueOf(System.currentTimeMillis());
+                ;
+                Uri newUri = ImageHelper.addBitmapToStorage(dir, fileName, b);
+                if (newUri == null) {
+                    Toast.makeText(this, getString(R.string.invalid_icon_from_uri),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    sendCancelResultAndFinish();
+                } else {
+                    Intent resultIntent = new Intent();
+                    resultIntent.setAction(INTENT_GALLERY_PICKER);
+                    resultIntent.putExtra("result", Activity.RESULT_OK);
+                    resultIntent.putExtra("uri", newUri.toString());
+                    sendBroadcastAsUser(resultIntent, UserHandle.CURRENT);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.invalid_icon_from_uri), Toast.LENGTH_SHORT)
+                        .show();
+                sendCancelResultAndFinish();
+            }
+        } else {
+            sendCancelResultAndFinish();
+        }
+    }
+
+    private void sendCancelResultAndFinish() {
+        Intent intent = new Intent(INTENT_GALLERY_PICKER);
+        intent.putExtra("result", Activity.RESULT_CANCELED);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+}

src/com/cardinal/wings/preference/ActionPreference.java
@@ -1,0 +1,134 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Simple preference class implementing ActionHolder interface to assign
+ * actions to buttons. It is ABSOLUTELY IMPERITIVE that the preference
+ * key is identical to the target ConfigMap tag in ActionConstants 
+ */
+
+package com.cardinal.wings.preference;
+
+import java.util.Map;
+
+import com.android.internal.utils.du.ActionConstants.ConfigMap;
+import com.android.internal.utils.du.ActionConstants.Defaults;
+import com.android.internal.utils.du.ActionHolder;
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.internal.utils.du.Config.ButtonConfig;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+
+public class ActionPreference extends Preference implements ActionHolder {
+    private Defaults mDefaults;
+    private ConfigMap mMap;
+    private ActionConfig mAction;
+    private ActionConfig mDefaultAction;
+
+    public ActionPreference(Context context) {
+        this(context, null);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ActionPreference(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs);
+    }
+
+    @Override
+    public String getTag() {
+        return this.getKey();
+    }
+
+    @Override
+    public void setTag(String tag) {
+        this.setKey(tag);
+    }
+
+    @Override
+    public Defaults getDefaults() {
+        return mDefaults;
+    }
+
+    @Override
+    public void setDefaults(Defaults defaults) {
+        mDefaults = defaults;
+        final String tag = this.getKey();
+        for (Map.Entry<String, ConfigMap> entry : defaults.getActionMap().entrySet()) {
+            if (((String) entry.getKey()).equals(tag)) {
+                mMap = entry.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public ConfigMap getConfigMap() {
+        return mMap;
+    }
+
+    @Override
+    public void setConfigMap(ConfigMap map) {
+        mMap = map;
+    }
+
+    @Override
+    public ButtonConfig getButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setButtonConfig(ButtonConfig button) {
+    }
+
+    @Override
+    public ActionConfig getActionConfig() {
+        return mAction;
+    }
+
+    @Override
+    public void setActionConfig(ActionConfig action) {
+        mAction = action;
+        this.setSummary(action.getLabel());
+    }
+
+    @Override
+    public ButtonConfig getDefaultButtonConfig() {
+        return null;
+    }
+
+    @Override
+    public void setDefaultButtonConfig(ButtonConfig button) {
+
+    }
+
+    @Override
+    public ActionConfig getDefaultActionConfig() {
+        return mDefaultAction;
+    }
+
+    @Override
+    public void setDefaultActionConfig(ActionConfig action) {
+        mDefaultAction = action;
+    }
+}